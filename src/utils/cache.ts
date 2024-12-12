fd248b789bd8ad8171442a26cd76a7b30c7b1cb6
Shady Abdelaziz Farahat <shady@itsacheckmate.com>
2024-12-12 16:03:39 +0400

    Add Cache Limit

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,11 +1,18 @@
 export class Cache {
   private store: Map<string, any>
-
+  //TODO:- ADD Limit to Cache
+  private limit: number
   constructor() {
     this.store = new Map()
+    this.limit = 200
   }
 
   public set(key: string, value: any) {
+    if (this.store.size >= this.limit) {
+      // Evict the first (oldest) entry to make space for the new one
+      const oldestKey = this.store.keys().next().value
+      oldestKey && this.store.delete(oldestKey)
+    }
     this.store.set(key, value)
   }
 
@@ -15,5 +22,9 @@ export class Cache {
 
   public clear() {
     this.store.clear()
+  }
+
+  delete(key: string) {
+    return this.store.delete(key)
   }
 }
